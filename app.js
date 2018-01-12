82abbd011e7781a52b5c04f6d691629fe7663bf0
Ivan <ivan3123708@gmail.com>
2018-01-12 08:55:48 +0100

    Regular update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -21,6 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: 'xxx', resave: false, saveUninitialized: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/product');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  Product.find((err, result) => {
+    res.render('index', { products: result });
+  });
+});
+
+router.get('/user/signup', (req, res, next) => {
+  res.render('user/signup');
+});
+
+router.post('/user/signup', (req, res, next) => {
+  res.redirect('/');
 });
 
 module.exports = router;
